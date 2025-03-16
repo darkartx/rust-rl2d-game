73041f03ba01633df4b3eadec60af31153b2611e
darkartx <darkartx@gmail.com>
2025-03-16 09:32:59 +0400

    *

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,39 @@ static PACKAGE_NAME: &str = env!("CARGO_PKG_NAME");
 
 static BACKGROUND_COLOR: Color = Color::new(0.3, 0.0, 0.0, 1.0);
 
+const MOVEMENT_SPEED: f32 = 200.0;
+const CIRCLE_RADIUS: f32 = 16.0;
+
 #[macroquad::main(get_config)]
 async fn main() {
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
 
     loop {
+        let delta_time = get_frame_time();
+
         clear_background(BACKGROUND_COLOR);
 
         if is_key_down(KeyCode::Right) {
-            x += 10.0;
+            x += MOVEMENT_SPEED * delta_time;
         }
 
         if is_key_down(KeyCode::Left) {
-            x -= 10.0;
+            x -= MOVEMENT_SPEED * delta_time;
         }
 
         if is_key_down(KeyCode::Up) {
-            y -= 10.0;
+            y -= MOVEMENT_SPEED * delta_time;
         }
 
         if is_key_down(KeyCode::Down) {
-            y += 10.0;
+            y += MOVEMENT_SPEED * delta_time;
         }
 
-        draw_circle(x, y, 16.0, YELLOW);
+        x = clamp(x, CIRCLE_RADIUS, screen_width() - CIRCLE_RADIUS);
+        y = clamp(y, CIRCLE_RADIUS, screen_height() - CIRCLE_RADIUS);
+
+        draw_circle(x, y, CIRCLE_RADIUS, YELLOW);
 
         next_frame().await
     }
